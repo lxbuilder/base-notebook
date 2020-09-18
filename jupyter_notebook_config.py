8fb5e160fd99b5738db38828f9456a6e968fbeb3
lxcpgitlab <wqerkljnwrklnqwreklwqre@oiejwqroiqewjroiqwerhj234.com>
2020-09-18 14:39:26 +0200

    enable authentication for v4

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -11,7 +11,6 @@ c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.NotebookApp.token = ''
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
